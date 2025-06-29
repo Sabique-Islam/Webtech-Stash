dde49b02c4dbeeee9bce844c98b7c99f91371d2c
sabique <sabiqueislam7@gmail.com>
2025-06-29 14:31:57 +0530

    conversion operation

---
+++ Miscellaneous/Javascript/Basics/04_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN : Not a Number
+
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
