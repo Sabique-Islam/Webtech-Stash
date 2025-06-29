12c9cf4c927828e1e7b87e5a2329e39c7e3de997
sabique <sabiqueislam7@gmail.com>
2025-06-29 14:39:28 +0530

    comparison

---
+++ Miscellaneous/Javascript/Basics/05_comparison.js
@@ -0,0 +1,18 @@
+/*
+
+== : equal to
+=== : equal to value and type  
+
+comparison convert null to a number (treating it as 0)
+*/
+
+// console.log(null>0) 
+// console.log(null==0)
+// console.log(null>=0)
+
+console.log(null == undefined)
+console.log(null === undefined)
+
+// console.log("0" == 0)
+// console.log("0" === 0)
+
