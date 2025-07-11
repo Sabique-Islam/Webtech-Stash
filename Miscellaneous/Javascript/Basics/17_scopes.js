e13072f8cf765f31eae7c3b0d0a7ed29c9212988
sabique <sabiqueislam7@gmail.com>
2025-07-11 19:16:12 +0530

    misc: scope is js

---
+++ Miscellaneous/Javascript/Basics/17_scopes.js
@@ -0,0 +1,8 @@
+let a = 300
+
+if(true){
+    let a = 10
+    console.log(`Inside "if" -> a = ${a}`);
+}
+
+console.log(`Outside "if" -> a = ${a}`);
---
+++ Miscellaneous/Javascript/Basics/18_scopes.js
@@ -0,0 +1,22 @@
+function one(){
+    let user = "Samosa"
+
+    function two(){
+        let web = "https://*.com"
+        console.log(user)
+        console.log(web)
+    }
+    //console.log(web)
+    two()
+}
+
+//one()
+
+if(true){
+    const a = 10
+    if(a===10){
+        const a = 100
+        console.log(a) 
+    }
+    console.log(a)
+}
